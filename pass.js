3e0cd9a8d5064212c7338f3770772234330a50f5
junaid-khannn <150054742+junaid-khannn@users.noreply.github.com>
2024-05-19 02:36:15 -0700

    Add files via upload

---
+++ pass.js
@@ -0,0 +1,11 @@
+
+
+function generatePassword() {
+    var length = 12,
+        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+?><:{}[]";
+    var password = "";
+    for (var i = 0, n = charset.length; i < length; ++i) {
+        password += charset.charAt(Math.floor(Math.random() * n));
+    }
+    document.getElementById("password").value = password;
+}
